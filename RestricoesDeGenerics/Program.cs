2da4595a58ca44513c10bb1d9cfc1e75e39d50fd
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-18 13:57:57 -0300

    Restricoes de Generics

---
+++ RestricoesDeGenerics/Program.cs
@@ -0,0 +1,49 @@
+﻿using RestricoesDeGenerics.Entities;
+using RestricoesDeGenerics.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace RestricoesDeGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Max:");
+            Console.WriteLine(max);
+        }
+    }
+}
+/*
+ PROBLEMA EXEMPLO: Uma empresa de consultoria deseja avaliar a performance de produtos,
+funcionários, dentre outras coisas. Um dos cálculos que ela precisa é encontrar
+o maior dentre um conjunto de elementos. Fazer um programa que leia um
+conjunto de N produtos, conforme exemplo, e depois mostre o mais caro deles.
+
+EXEMPLO DE SAÍDA:
+
+Enter N: 3
+Computer,890.50
+IPhone X,910.00
+Tablet,550.00
+Max:
+IPhone X, 910.00
+ */
---
+++ RestricoesDeGenerics/Services/CalculationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestricoesDeGenerics.Services
+{
+    class CalculationService
+    {
+        public T Max<T>(List<T> list) where T : IComparable //Para declarar um método como tipo genérico seu tipo deve ser do tipo "T" e na frente do nome do método também tem que se declarar que é do tipo T entre <>: "<T>". E no trecho (where T : IComparable) declaro que esse tipo T tem que ser comparável com outro entrando no assunto da aula de restrições de Generics.
+        { /*
+            * OUTRAS POSSIBLIDADES DE RESTRIÇÕES:
+            
+            - where T : struct
+            - where T : class
+            - where T : unmanaged
+            - where T : new()
+            - where T : <base type name>
+            - where T : U
+           */
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty");
+            }
+
+            T max = list[0]; //Aqui o tipo da variável max também tem que ser do tipo "T"
+            for (int i=1; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(max) > 0) //Aqui entra a funcionalidade da interface IComparable (CompareTo) que retorna valores int -1, 0 e 1)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
